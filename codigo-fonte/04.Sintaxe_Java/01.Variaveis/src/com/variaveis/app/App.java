9c3c38aeda4f4ee7e9f8ebb5bc6bca48e4cb0040
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-08-05 18:50:51 -0300

    Atualizado: Sintaxe Java.

--- codigo-fonte/04.Sintaxe_Java/01.Variaveis/src/com/variaveis/app/App.java
+++ codigo-fonte/04.Sintaxe_Java/01.Variaveis/src/com/variaveis/app/App.java
@@ -1,6 +1,15 @@
 package com.variaveis.app;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Fulano";
+        int idade = 30;
+        double altura = 1.75;
+        boolean doador = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(doador);
     }
 }
