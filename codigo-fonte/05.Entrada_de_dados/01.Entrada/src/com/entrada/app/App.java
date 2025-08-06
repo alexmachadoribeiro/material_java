0d302ba03590c12de0f83c0a4772fffa54ebc496
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-08-06 13:50:56 -0300

    Adicionado: Entrada de dados.

---
+++ codigo-fonte/05.Entrada_de_dados/01.Entrada/src/com/entrada/app/App.java
@@ -0,0 +1,26 @@
+package com.entrada.app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // instancia a classe Scanner
+        Scanner leia = new Scanner(System.in);
+
+        // declaração de variáveis
+        String nome;
+        int idade;
+        double altura;
+
+        // input
+        System.out.println("Informe seu nome:");
+        nome = leia.nextLine();
+        System.out.println("Informe sua idade:");
+        idade = leia.nextInt();
+        System.out.println("Informe sua altura:");
+        altura = leia.nextDouble();
+
+        // fecha objeto do tipo Scanner
+        leia.close();
+    }
+}
